e2067137fcc376e739d50329ffffde12179aaad1
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2022-03-31 12:24:15 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ const routes = require("./routes");
 app.use(routes);
 
 const port = process.env.APP_PORT || 3333;
-server.listen(port, () => {
-  console.log(`[app.js] > Server running in ${process.env.APP_URL}`);
+app.listen(port, () => {
+  console.log(`[app.js] > Server running in ${process.env.APP_URL}:${port}`);
 });
